8b4b0f84eacbf3c56323645f475893692fe3de84
Happy <nphuc22125@gmail.com>
2023-02-21 19:19:32 +0700

    ver2

--- js/index.js
+++ js/index.js
@@ -23,107 +23,57 @@ function kiemTraSoDuong() {
 // bài 1 :tổng số dương
 // output
 document.getElementById('btn-in-so-duong').onclick = function () {
-
-    document.getElementById('in-so-duong').innerHTML = tongSoDuong();
-}
-// process
-function tongSoDuong() {
-    var tongSoDuong = 0;
-    for (i = 0; i < arrNum.length; i++) {
-        if (arrNum[i] > 0) {
-            tongSoDuong += Number(arrNum[i])
-        }
-    }
-    return tongSoDuong;
+    // process
+    var tongDuong = tongSoDuong(arrNum);
+    // output
+    document.getElementById('in-so-duong').innerHTML = tongDuong;
 }
 // bài 2 : đếm số dương
 
 document.getElementById('btn-dem-so-duong').onclick = function () {
+    // process
+    var demDuong = demSoDuong(arrNum);
     // output
-    document.getElementById('dem-so-duong').innerHTML = demSoDuong();
-    function demSoDuong() {
-        var dem = 0;
-        for (i = 0; i < arrNum.length; i++) {
-            if (arrNum[i] > 0) {
-                dem += 1;
-            }
-        }
-        return dem;
-    }
+    document.getElementById('dem-so-duong').innerHTML = demDuong;
 }
 // bài 3
 document.getElementById('btn-tim-so-nho').onclick = function () {
-    //output
-    document.getElementById('tim-so-nho').innerHTML = timSoNhoNhat();
     // process
-    function timSoNhoNhat() {
-        var soNho = arrNum[0]
-        for (i = 1; i < arrNum.length; i++) {
-            if (soNho > arrNum[i]) {
-                soNho = arrNum[i];
-            }
-        }
-        return soNho
-    }
+    var timNhoNhat = timSoNhoNhat(arrNum)
+    //output
+    document.getElementById('tim-so-nho').innerHTML = timNhoNhat;
 }
 // bài 4
 document.getElementById('btn-tim-so-duong-nho').onclick = function () {
+    // input
+    var find = timSoDuong(arrNum);
     // output
-    document.getElementById('tim-so-duong').innerHTML = timSoDuong();
-    if (timSoDuong().length > 0) {
-        document.getElementById('tim-so-duong-nho').innerHTML = timSoDuongNhoNhat();
+    var soDuongNho = find[0];
+    // process
+    for (i = 1; i < find.length; i++) {
+        if (soDuongNho > find[i]) {
+            soDuongNho = find[i];
+        }
+    }
+    document.getElementById('tim-so-duong').innerHTML = find;
+    if (find.length > 0) {
+        document.getElementById('tim-so-duong-nho').innerHTML = soDuongNho;
     }
     else {
-        document.getElementById('tim-so-duong-nho').innerHTML = ' Không có phần tử dương'
+        document.getElementById('tim-so-duong-nho').innerHTML = ' Không có phần tử dương';
     }
-    // process
-    function timSoDuong() {
-        var arrDuong = [];
-        var soDuong = '';
-        for (i = 0; i < arrNum.length; i++) {
-            if (arrNum[i] > 0) {
-                soDuong = arrNum[i];
-                arrDuong.push(soDuong);
-            }
-        }
-        return arrDuong;
-    }
-
-    function timSoDuongNhoNhat() {
-        var find = timSoDuong();
-        var soDuongNho = find[0];
-        for (i = 1; i < find.length; i++) {
-            if (soDuongNho > find[i]) {
-                soDuongNho = find[i];
-            }
-        }
-        return soDuongNho;
-    }
-
 }
-
 // bài 5
 document.getElementById('btn-tim-so-chan').onclick = function () {
-
-    document.getElementById('tim-so-chan-cuoi').innerHTML = timChanCuoi();
-    document.getElementById('tim-so-chan').innerHTML = timSoChan();
-    // process
-    function timSoChan() {
-        var arrChan = [];
-        var soChan = '';
-        for (i = 0; i < arrNum.length; i++) {
-            if (arrNum[i] % 2 === 0) {
-                soChan = arrNum[i];
-                arrChan.push(soChan);
-            }
-        }
-        return arrChan;
+    var timCuoi = timSoChan(arrNum);
+    var soChanCuoi;
+    if (timCuoi.length === 0) {
+        document.getElementById('tim-so-chan-cuoi').innerHTML = '-1'
     }
-
-    function timChanCuoi() {
-        var timCuoi = timSoChan();
-        var soChanCuoi = timCuoi[timCuoi.length - 1];
-        return soChanCuoi;
+    else {
+        soChanCuoi = timCuoi[timCuoi.length - 1];
+        document.getElementById('tim-so-chan').innerHTML = timCuoi;
+        document.getElementById('tim-so-chan-cuoi').innerHTML = soChanCuoi;
     }
 }
 //**********bài 6***************
@@ -131,67 +81,36 @@ document.getElementById('btn-doi-cho').onclick = function () {
     // input
     var soMot = Number(document.getElementById('so-thu-nhat').value);
     var soHai = Number(document.getElementById('so-thu-hai').value);
-    // output
-    document.getElementById('doi-cho').innerHTML = '[' + doiCho() + ']';
-
     // process
-    function doiCho() {
-        var swap;
-        swap = arrNum[soMot];
-        arrNum[soMot] = arrNum[soHai];
-        arrNum[soHai] = swap;
-        return arrNum;
-    }
+    var doiViTri = doiCho(arrNum, soMot, soHai);
+    // output
+    document.getElementById('doi-cho').innerHTML = '[' + doiViTri + ']';
 }
 // bài 7
 document.getElementById('btn-sap-xep').onclick = function () {
 
-    document.getElementById('sap-xep').innerHTML = sapxep();
+    var sapXepTang = sapxep(arrNum);
+    document.getElementById('sap-xep').innerHTML = sapXepTang;
 
-    function sapxep() {
-        arrNum.sort(function (a, b) {
-            return a - b;
-        });
-        return arrNum;
-    }
+
 }
 // ************* Bài 8 ***********************
 document.getElementById('btn-nguyen-to').onclick = function () {
+
+    // process
+    var nguyenToDau = 0;
+    for (i = 0; i < arrNum.length; i++) {
+        if (kiemTraNguyenTo(arrNum[i]) === true) {
+            nguyenToDau = arrNum[i];
+            break;
+        }
+    }
     // output
-    if (timNguyenToDau() === 0) {
+    if (nguyenToDau === 0) {
         document.getElementById('nguyen-to').innerHTML = '-1'
     }
     else {
-        document.getElementById('nguyen-to').innerHTML = timNguyenToDau();
-    }
-    // process
-    function kiemTraNguyenTo(n) {
-        var count = true;
-        if (n < 2) {
-            count = false;
-        }
-        else {
-            for (uoc = 2; uoc <= Math.sqrt(n); uoc++) {
-                if (n % uoc === 0) {
-                    count = false;
-                    break;
-                }
-            }
-            if (uoc === 2) {
-                output = true;
-            }
-        }
-        return count;
-    }
-    function timNguyenToDau() {
-        var nguyenToDau = 0;
-        for (i = 0; i < arrNum.length; i++) {
-            if (kiemTraNguyenTo(arrNum[i]) === true) {
-                nguyenToDau = arrNum[i];
-                break;
-            }
-        }
-        return nguyenToDau;
+        document.getElementById('nguyen-to').innerHTML = nguyenToDau;
     }
 }
 // bài 9
@@ -206,52 +125,30 @@ document.getElementById('btn-them-gia-tri-thuc').onclick = function () {
     document.getElementById('in-so-thuc').innerHTML = '[' + arrNum + ']';
 }
 document.getElementById('btn-so-nguyen').onclick = function () {
-    // output
-    document.getElementById('so-nguyen').innerHTML = demSoNguyen();
-
     // process
-    function demSoNguyen() {
-        var demNguyen = 0;
-        for (i = 0; i < arrNum.length; i++) {
-            if (Number.isInteger(Number(arrNum[i])) == true) {
-                demNguyen += 1;
-            }
-        }
-        return demNguyen;
-    }
+    var soNguyen = demSoNguyen(arrNum);
+    // output
+    document.getElementById('so-nguyen').innerHTML = soNguyen;
 }
 
 // bài 10
 document.getElementById('btn-so-sanh').onclick = function () {
+    
+    // process
+    var soSanhDuong = demDuong(arrNum);
+    var soSanhAm = demAm(arrNum);
     // output
-    if(demDuong()==demAm()){
+    if (soSanhDuong == soSanhAm) {
         document.getElementById('so-sanh').innerHTML = 'Số Dương Bằng Số Âm';
     }
-    else if(demDuong()>demAm()){
+    else if (soSanhDuong > soSanhAm) {
         document.getElementById('so-sanh').innerHTML = 'Số Dương Lớn Hơn Số Âm';
     }
-    else{
+    else {
         document.getElementById('so-sanh').innerHTML = 'Số Dương Bé Hơn Số Âm';
     }
     // process
-    function demDuong() {
-        var demDuong = 0;
-        for (i = 0; i < arrNum.length; i++) {
-            if (arrNum[i] > 0) {
-                demDuong += 1;
-            }
-        }
-        return demDuong;
-    }
-    function demAm() {
-        var demAm = 0;
-        for (i = 0; i < arrNum.length; i++) {
-            if (arrNum[i] < 0) {
-                demAm += 1;
-            }
-        }
-        return demAm;
-    }
+
 
 }
 
--- js/method.js
+++ js/method.js
@@ -1,4 +1,175 @@
 
-// ******************
+/**
+ * Hàm xử lý nhận vào 1 mảng và tính tổng giá trị dương có trong mảng
+ * @param {*} arrNum : là mảng cần nhập vào 
+ * @returns : trả về tổng giá trị các số dương có trong mảng.
+ */
+function tongSoDuong(arrNum) {
+    var tongSoDuong = 0;
+    for (i = 0; i < arrNum.length; i++) {
+        if (arrNum[i] > 0) {
+            tongSoDuong += Number(arrNum[i])
+        }
+    }
+    return tongSoDuong;
+}
+/**
+ * Hàm xử lý nhận vào 1 mảng và đếm số lượng số dương có trong mảng
+ * @param {*} arrNum :là màng cần nhập vào
+ * @returns :  trả về số lượng số dương có trong mảng
+ */
+function demSoDuong(arrNum) {
+    var dem = 0;
+    for (i = 0; i < arrNum.length; i++) {
+        if (arrNum[i] > 0) {
+            dem += 1;
+        }
+    }
+    return dem;
+}
+/**
+ * Hàm xử lý nhận vào 1 mảng và tìm số nhỏ nhất của mảng vừa nhập
+ * @param {*} arrNum : là mảng cần nhập vào
+ * @returns : trả về số nhỏ nhất trong mảng 
+ */
+function timSoNhoNhat(arrNum) {
+    var soNho = arrNum[0]
+    for (i = 1; i < arrNum.length; i++) {
+        if (soNho > Number(arrNum[i])) {
+            soNho = Number(arrNum[i]);
+        }
+    }
+    return soNho
+}
 
-// *******************
+/**
+ * Hàm xử lí nhập vào 1 mảng , tìm số dương có trong mảng , và trả về mảng mới chỉ chứa số dương.
+ * @param {*} arrNum :  mảng cần nhập vào
+ * @returns :  trả về mảng mới chỉ chứa số dương
+ */
+function timSoDuong(arrNum) {
+    var arrDuong = [];
+    var soDuong = '';
+    for (i = 0; i < arrNum.length; i++) {
+        if (arrNum[i] > 0) {
+            soDuong = arrNum[i];
+            arrDuong.push(soDuong);
+        }
+    }
+    return arrDuong;
+}
+
+
+/**
+ * hàm xử lí nhận vào mảng và tìm số chẳn có trong mảng
+ * @param {*} arrNum : là mảng cần nhập vào
+ * @returns : trả về mảng mới chỉ chứa số chẳn
+ */
+function timSoChan(arrNum) {
+    var arrChan = [];
+    var soChan = '';
+    for (i = 0; i < arrNum.length; i++) {
+        if (arrNum[i] % 2 === 0) {
+            soChan = arrNum[i];
+            arrChan.push(soChan);
+        }
+    }
+    return arrChan;
+}
+
+/**
+ *  hàm nhận  xử lí vào mảng , giá trị a , b và xử lí đổi chỗ các phần tử trong mảng dựa vào giá trị a ,b  
+ * @param {*} arrNum : là mảng cần nhập vào
+ * @param {*} a : giá trị thứ nhất
+ * @param {*} b  : giá trị thứ hai
+ * @returns  : trả về mảng mới sao khi đổi chỗ 
+ */
+function doiCho(arrNum,a,b) {
+    var swap;
+    swap = arrNum[a];
+    arrNum[a] = arrNum[b];
+    arrNum[b] = swap;
+    return arrNum;
+}
+
+/**
+ * Hàm nhận vào một mảng và sắp xếp tăng dần mảng vừa nhận
+ * @param {*} arrNum : mảng cần nhập vào
+ * @returns : trả về mảng mới sao khi sắp xếp
+ */
+function sapxep(arrNum) {
+    arrNum.sort(function (a, b) {
+        return a - b;
+    });
+    return arrNum;
+}
+
+/**
+ * hàm nhận giá trị và kiểm tra giá trị đó có phải số nguyên tố hay không
+ * @param {*} arrNum : giá trị nhập vào
+ * @returns : trả về true nếu là số nguyên tố ngược lại thì trả về false
+ */
+function kiemTraNguyenTo(arrNum) {
+    var count = true;
+    if (arrNum < 2) {
+        count = false;
+    }
+    else {
+        for (uoc = 2; uoc <= Math.sqrt(arrNum); uoc++) {
+            if (arrNum % uoc === 0) {
+                count = false;
+                break;
+            }
+        }
+        if (uoc === 2) {
+            output = true;
+        }
+    }
+    return count;
+}
+
+/**
+ *  hàm nhận vào mảng và đếm trong mảng có bao nhiêu số nguyên
+ * @param {*} arrNum : mảng cần nhập vào
+ * @returns : trả về số lượng số nguyên có trong mảng
+ */
+function demSoNguyen(arrNum) {
+    var demNguyen = 0;
+    for (i = 0; i < arrNum.length; i++) {
+        if (Number.isInteger(Number(arrNum[i])) == true) {
+            demNguyen += 1;
+        }
+    }
+    return demNguyen;
+}
+
+/**
+ * hàm nhận vào một mảng và đếm trong mảng có bao nhiêu số dương
+ * @param {*} arrNum : mảng cần nhập vào
+ * @returns : trả về  số lượng số dương có trong mảng
+ */
+function demDuong(arrNum) {
+    var demDuong = 0;
+    for (i = 0; i < arrNum.length; i++) {
+        if (arrNum[i] > 0) {
+            demDuong += 1;
+        }
+    }
+    return demDuong;
+}
+
+
+/**
+ * hàm nhận vào một mảng và đếm trong mảng có bao nhiêu số âm
+ * @param {*} arrNum : mảng cần nhập vào
+ * @returns :  trả về số lượng số âm có trong mảng
+ */
+function demAm() {
+    var demAm = 0;
+    for (i = 0; i < arrNum.length; i++) {
+        if (arrNum[i] < 0) {
+            demAm += 1;
+        }
+    }
+    return demAm;
+}
